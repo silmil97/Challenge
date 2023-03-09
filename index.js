2dcb164fcdb673523ab97ba4198c50ea4766f49e
silmil97 <85782295+silmil97@users.noreply.github.com>
2023-03-09 19:09:55 +0100

    feat: hardcode bucketlist

--- index.js
+++ index.js
@@ -1,61 +1,42 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 
-const books = [
+const buckets = [
   {
-    title: "The Awakening",
-    author: "Kate Chopin",
+    name: "The Awakening",
+    creationDate: "2019-12-11T23:32:47+00:00",
   },
   {
-    title: "City of Glass",
-    author: "Paul Auster",
+    name: "City of Glass",
+    creationDate: "2029-12-11T23:32:47+00:00",
   },
 ];
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type Bucket {
+    name: String
+    creationDate: String
   }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    buckets: [Bucket]
   }
 `;
 
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    buckets: () => buckets,
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-
 async function getUrl() {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
-  console.log({ url });
+  console.log(`🚀  Server ready at: ${url}`);
 }
 getUrl();
